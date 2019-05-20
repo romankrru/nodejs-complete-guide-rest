25dd2b0c240a6b00a496f3c4b6b5369594b69e00
romankrru <rm07ru@gmail.com>
2019-05-20 13:30:44 +0300

    Setup frontend deps

---
+++ frontend/src/Feed/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container, Header} from 'semantic-ui-react';
+
+const Feed = props => {
+	return (
+		<Container>
+			<Header>This is my app</Header>
+		</Container>
+	);
+};
+
+export default Feed;
