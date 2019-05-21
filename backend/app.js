ce5c895244e60830a9ca00ad28dccc6356c15299
romankrru <rm07ru@gmail.com>
2019-05-22 00:53:12 +0300

    Setup Post model

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
+	process.env.MONGO_PASSWORD
+}@cluster0-22nab.mongodb.net/messages?retryWrites=true`;
 
 const app = express();
 
@@ -24,4 +33,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
-app.listen(1337);
+
+mongoose
+	.connect(MONGODB_URI, {useNewUrlParser: true})
+
+	.then(() => {
+		console.log('Server started at http://localhost:1337');
+		app.listen(1337);
+	})
+
+	.catch(err => console.error(err));
